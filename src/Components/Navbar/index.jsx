8749792d7fe2d8b2aa34179d0e18c98957279a18
AxelJaBo <axel.910921@gmail.com>
2023-10-27 21:07:34 -0700

    Componente Navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,81 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink
+                        to='/'
+                    >
+                        Shopi
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/'
+                    >
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/clothes'
+                    >
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/electronics'
+                    >
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/furnitures'
+                    >
+                        Furnitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/toys'
+                    >
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/others'
+                    >
+                        Others
+                    </NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    axel@gmail.com
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-orders'
+                    >
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-account'
+                    >
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    Carrito
+                </li>
+            </ul>
+        </nav>
+    )
+}
