c48a3159725eb4814c9d1632e5436cbd18f9be37
AxelJaBo <axel.910921@gmail.com>
2023-11-08 21:36:27 -0800

    Eliminar productos del carrito

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,9 +1,10 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
-    const { id, title, imageUrl, price} = props
+    const { id, title, imageUrl, price, handleDelete } = props
+
     return (
-        <div className="flex justify-between place-items-center mb-3">
+        <div className="flex justify-between items-center mb-3">
             <div className='flex items-center gap-2'>
                 <figure className='w-20 h-20'>
                     <img className='w-full h-full rounded-lg object-cover' src={imageUrl} alt={title} />
@@ -12,7 +13,7 @@ const OrderCard = props => {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>{price}</p>
-                <XMarkIcon className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
+                <XMarkIcon onClick={() => handleDelete(id)} className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
             </div>
         </div>
     )
